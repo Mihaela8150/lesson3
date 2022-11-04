3fc3554846d6621056db6e292cd493b675f60098
Mihaela8150 <114097144+Mihaela8150@users.noreply.github.com>
2022-11-04 20:26:45 +0200

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,31 @@
+let fruits = ["orange", "apple", "mango"]
+
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+fruits.push("grapefruit")
+console.log(fruits)
+
+function mihaela() {
+    console.log("hi")
+}
+
+mihaela()
+
+
+let person = {
+    name:"Ana",
+    age:"13",
+    description:"sweet",
+    sound:"hello",
+    note:"10"
+
+   
+
+}
+console.log(person.name)
+console.log(person.description)
+console.log(person.sound)
+console.log(person.note)
+delete person.age
+
